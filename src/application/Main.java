40e5d4c2897001a374ab9f099745277bf66c506e
IgnacioMV <nmvmm22@gmail.com>
2017-06-13 01:36:06 +0200

    Small Progress

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,15 @@
 package application;
 	
-import java.awt.event.MouseEvent;
+
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventDispatchChain;
+import javafx.event.EventDispatcher;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -15,8 +19,10 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeCell;
@@ -24,21 +30,26 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	private final TabPane tabPane2 = new TabPane();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.setTitle("GM Swiss Knife");
 			
-			
 			final ObservableList<String> listItems = FXCollections.observableArrayList("Campaign 1", "Campaign 2", "Campaign 3");
 
 			
-			TreeItem<String> dummyRoot = new TreeItem<>();
+			TreeItem<String> dummyRoot1 = new TreeItem<>();
+			TreeItem<String> dummyRoot2 = new TreeItem<>();
 			
 	        for (int i = 0; i < listItems.size(); i++) {
 	        	
@@ -46,46 +57,74 @@ public class Main extends Application {
 	        	rootItem.setExpanded(false);
 	        	
 	            TreeItem<String> pcs = new TreeItem<String> ("PCs");
+	            pcs.setExpanded(false);
 	            TreeItem<String> pc1 = new TreeItem<String> ("PC1");
 	            TreeItem<String> pc2 = new TreeItem<String> ("PC2");
 	            
 	            pcs.getChildren().addAll(pc1, pc2);
 	            
 	            TreeItem<String> npcs = new TreeItem<String> ("NPCs");
+	            TreeItem<String> npc1 = new TreeItem<String> ("NPC1");
+	            TreeItem<String> npc2 = new TreeItem<String> ("NPC2");
 	            
-	            
+	            npcs.getChildren().addAll(npc1, npc2);
+	       
 	            TreeItem<String> locations = new TreeItem<String> ("Locations");
-	            rootItem.getChildren().add(pcs);
-	            rootItem.getChildren().add(npcs);
-	            rootItem.getChildren().add(locations);
-	            dummyRoot.getChildren().add(rootItem);
+	            rootItem.getChildren().addAll(pcs, npcs, locations);
+	            dummyRoot1.getChildren().add(rootItem);
+	            
+	            if (dummyRoot2.getChildren().isEmpty()) {
+	            	dummyRoot2.getChildren().addAll(pcs, npcs, locations);
+	            }
 	        }        
-	        TreeView<String> tree = new TreeView<String>(dummyRoot);
-	        tree.setEditable(true);
-	        tree.setCellFactory(new Callback<TreeView<String>,TreeCell<String>>(){
+	        TreeView<String> tree1 = new TreeView<String>(dummyRoot1);
+	        //tree1.setEditable(true);
+	        tree1.setCellFactory(new Callback<TreeView<String>,TreeCell<String>>(){
 	            @Override
 	            public TreeCell<String> call(TreeView<String> p) {
-	                return new TextFieldTreeCellImpl();
+	            	
+	            	TextFieldTreeCellImpl cell = new TextFieldTreeCellImpl((p.getChildrenUnmodifiable().isEmpty()) ? true : false);
+	                
+	                return cell;
 	            }
 	        });
-	        tree.setShowRoot(false);
-			tree.setPrefWidth(150);
+	        tree1.setShowRoot(false);
+			
+			TreeView<String> tree2 = new TreeView<String>(dummyRoot2);
+			tree2.setEditable(true);
+	        tree2.setCellFactory(new Callback<TreeView<String>,TreeCell<String>>(){
+	            @Override
+	            public TreeCell<String> call(TreeView<String> p) {
+	                return new TextFieldTreeCellImpl(true);
+	            }
+	        });
+	        tree2.setShowRoot(false);
+			
+			TabPane tabPane1 = new TabPane();
+			Tab campaigns = new Tab("Campaigns");
+			Tab resources = new Tab("Resources");
+			campaigns.setContent(tree1);
+			resources.setContent(tree2);
+			tabPane1.getTabs().addAll(campaigns, resources);
+			tabPane1.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 			
 			final TextArea textArea = new TextArea("");
 			textArea.setWrapText(true);
 			
-			TabPane tabPane = new TabPane();
 			Tab tab = new Tab();
 			tab.setText("new tab");
 			tab.setContent(textArea);
-			tabPane.getTabs().add(tab);
+			tabPane2.getTabs().add(tab);
 			
-			BorderPane.setAlignment(tree, Pos.TOP_LEFT);
-			BorderPane.setAlignment(tabPane, Pos.CENTER);
+			SplitPane root = new SplitPane();
+			root.getItems().addAll(tabPane1, tabPane2);
+			root.setDividerPosition(0, 0.2f);
+			//BorderPane.setAlignment(tree, Pos.TOP_LEFT);
+			//BorderPane.setAlignment(tabPane, Pos.CENTER);
 			
-			BorderPane root = new BorderPane(tabPane, null, null, null, tree);
+			//BorderPane root = new BorderPane(tabPane, null, null, null, tree);
 			
-			Scene scene = new Scene(root, 640, 480);
+			Scene scene = new Scene(root, 1024, 768);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -96,14 +135,28 @@ public class Main extends Application {
 	private final class TextFieldTreeCellImpl extends TreeCell<String> {
 		 
         private TextField textField;
+        private boolean isFolder;
  
-        public TextFieldTreeCellImpl() {
+        public TextFieldTreeCellImpl(boolean isFolder) {
+        	this.isFolder = isFolder;
+        	addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+                @Override public void handle(MouseEvent t) {
+                    if (t.getButton() == MouseButton.PRIMARY && t.getClickCount() == 2) {
+                    	if(treeItemProperty().getValue().getChildren().isEmpty()) {
+                    		final TextArea textArea = new TextArea("");
+                    		textArea.setWrapText(true);
+                    		textArea.setText("This is a new file for "+getItem().toString());
+                    		Tab newTab = new Tab(getItem().toString());
+                    		newTab.setContent(textArea);
+                    		tabPane2.getTabs().add(newTab);
+        				}
+                    	System.out.println("double click");
+                    	t.consume();
+                    }
+                }
+            });
         }
-        
-        public void onMouseClicked(MouseEvent click) {
-        	
-        }
- 
+         
         @Override
         public void startEdit() {
             super.startEdit();
@@ -126,10 +179,12 @@ public class Main extends Application {
         @Override
         public void updateItem(String item, boolean empty) {
             super.updateItem(item, empty);
- 
+            System.out.println(getTreeItem());
+            
             if (empty) {
                 setText(null);
                 setGraphic(null);
+                
             } else {
                 if (isEditing()) {
                     if (textField != null) {
@@ -140,8 +195,13 @@ public class Main extends Application {
                 } else {
                     setText(getString());
                     setGraphic(getTreeItem().getGraphic());
+                    
                 }
             }
+        }
+        
+        public boolean getIsFolder(){
+        	return this.isFolder;
         }
  
         private void createTextField() {
@@ -150,16 +210,15 @@ public class Main extends Application {
  
                 @Override
                 public void handle(KeyEvent t) {
-                    if (t.getCode() == KeyCode.F2) {
+                    if (t.getCode() == KeyCode.ENTER) {
                         commitEdit(textField.getText());
-                    } else if (t.getCode() == KeyCode.F2) {
-                        cancelEdit();
                     }
                 }
             });
-            
            
         }
+        
+        
  
         private String getString() {
             return getItem() == null ? "" : getItem().toString();
@@ -169,4 +228,28 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+}
+
+class TreeMouseEventDispatcher implements EventDispatcher {
+    private final EventDispatcher originalDispatcher;
+
+    public TreeMouseEventDispatcher(EventDispatcher originalDispatcher) {
+      this.originalDispatcher = originalDispatcher;
+    }
+
+    public Event dispatchEvent(Event event, EventDispatchChain tail) {
+        if (event instanceof MouseEvent) {
+           if (((MouseEvent) event).getButton() == MouseButton.PRIMARY
+               && ((MouseEvent) event).getClickCount() >= 2) {
+
+             if (!event.isConsumed()) {
+               // Implement your double-click behavior here, even your
+               // MouseEvent handlers will be ignored, i.e., the event consumed!
+             }
+
+             event.consume();
+           }
+        }
+        return originalDispatcher.dispatchEvent(event, tail);
+    }
 }
